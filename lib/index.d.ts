8a82160738dfee1505094b17c14ba32a25a7ba16
Ilya Shlyakhovoy <bulgarus@inbox.ru>
2019-11-30 00:08:58 +0300

    rm lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "./interface";
-import { IReverseGeocode, ILocation, IGeocode } from "./interface";
-export default class ReverseGeocode {
-    private options;
-    constructor(options?: IReverseGeocode);
-    locate(coords: ILocation): Promise<IGeocode>;
-}
