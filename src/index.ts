ee04cabec79d0f168fcea21ba70d31b0ddd22952
Ilya Shlyakhovoy <bulgarus@inbox.ru>
2019-11-30 00:08:11 +0300

    prepublish ready

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,29 @@
-import axios from "axios";
-export * from "./interface";
-import { IReverseGeocode, ILocation, IGeocode } from "./interface";
+import axios from 'axios';
+export * from './interface';
+import { IGeocode, ILocation, IReverseGeocode } from './interface';
 
 export default class ReverseGeocode {
   constructor(
     private options: IReverseGeocode = {
-      api: "https://api.bigdatacloud.net",
-      endpoint: "/data/reverse-geocode-client",
-      language: "en",
-      format: "latitude=${lat}&longitude=${long}&localityLanguage=${lang}"
-    }
+      api: 'https://api.bigdatacloud.net',
+      endpoint: '/data/reverse-geocode-client',
+      format: 'latitude=${lat}&longitude=${long}&localityLanguage=${lang}',
+      language: 'en',
+    },
   ) {}
   public async locate(coords: ILocation): Promise<IGeocode> {
-    const queryString = (this.options.format || "").replace(
-      /(\${lat})|(\${long})|(\${lang})/g,
-      match => {
-        switch (match) {
-          case "${lat}":
-            return String(coords.lat);
-          case "${long}":
-            return String(coords.long);
-          case "${lang}":
-            return this.options.language || "en";
-        }
-        return "";
+    const queryString = (this.options.format || '').replace(/(\${lat})|(\${long})|(\${lang})/g, match => {
+      switch (match) {
+        case '${lat}':
+          return String(coords.lat);
+        case '${long}':
+          return String(coords.long);
+        case '${lang}':
+          return this.options.language || 'en';
       }
-    );
-    const answer = await axios.get(
-      `${this.options.api}${this.options.endpoint}?${queryString}`
-    );
+      return '';
+    });
+    const answer = await axios.get(`${this.options.api}${this.options.endpoint}?${queryString}`);
     return answer.data;
   }
 }
