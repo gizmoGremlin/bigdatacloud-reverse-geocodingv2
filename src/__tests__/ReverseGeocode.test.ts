b525e8728245a0a0927e27fc23f4723ea882a161
nick gill <nick.gilliosipescu@gmail.com>
2020-06-23 14:07:20 -0300

    fix test

--- src/__tests__/ReverseGeocode.test.ts
+++ src/__tests__/ReverseGeocode.test.ts
@@ -1,9 +1,21 @@
 import ReverseGeocode from '../index';
 test('Test reverse geocode of the location 0,0', async () => {
   const geocode = new ReverseGeocode();
+  console.log(await geocode.locate({ lat: 0, long: 0 }));
+
   expect(await geocode.locate({ lat: 0, long: 0 })).toStrictEqual({
     latitude: 0,
-    locality: 'Atlantic Ocean',
+    longitude: 0,
+    localityLanguageRequested: 'en',
+    continent: '',
+    continentCode: '',
+    countryName: '',
+    countryCode: '__',
+    principalSubdivision: '',
+    principalSubdivisionCode: '',
+    city: '',
+    locality: 'Null Island',
+    postcode: '',
     localityInfo: {
       informative: [
         {
@@ -12,9 +24,13 @@ test('Test reverse geocode of the location 0,0', async () => {
           order: 0,
           wikidataId: 'Q97',
         },
+        {
+          description: 'fictional island at 0°N 0°E',
+          name: 'Null Island',
+          order: 1,
+          wikidataId: 'Q16896007',
+        },
       ],
     },
-    localityLanguageRequested: 'en',
-    longitude: 0,
   });
 });
